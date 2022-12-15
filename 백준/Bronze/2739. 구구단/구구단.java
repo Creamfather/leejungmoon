19a5a03007779bb7902b81db768ef6ea8d2b8746
Creamfather <118109962+Creamfather@users.noreply.github.com>
2022-12-15 14:37:47 +0900

    [Bronze V] Title: 구구단, Time: 252 ms, Memory: 18668 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n;
+		n = sc.nextInt();
+		for (int i = 1; i <= 1; i++) {
+			for( int k = 1; k <=9; k++) {
+				System.out.println(n + " * " + k + " = " + (n*k));
+			}
+		}
+		
+		
+	}
+
+}
