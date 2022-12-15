facf8af22b9d386d947ddbe310c7dff4b042ddfc
Creamfather <118109962+Creamfather@users.noreply.github.com>
2022-12-15 14:43:46 +0900

    [Bronze IV] Title: R2, Time: 204 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// (r1+x)/2 = s
+		Scanner sc = new Scanner(System.in);
+		int r = sc.nextInt();
+		int x = sc.nextInt();
+		
+		System.out.println((x*2)-r);
+		
+		
+	}
+
+}
