68f759b44ac0abfa1bf66a5cdcf6135667c3f594
Creamfather <118109962+Creamfather@users.noreply.github.com>
2022-12-15 15:01:29 +0900

    [Bronze V] Title: A+B - 8, Time: 276 ms, Memory: 18956 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		//각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
+		int N = sc.nextInt();
+		for (int i = 1; i <= N; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println("Case #" + i + ": " + a + " + " + b + " = " + (a + b));
+		}
+
+	}
+
+}
