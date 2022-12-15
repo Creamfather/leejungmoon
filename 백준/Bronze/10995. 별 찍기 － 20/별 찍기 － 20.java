c17aebb52b8e4960ba13bde76993e1a2395dcf80
Creamfather <118109962+Creamfather@users.noreply.github.com>
2022-12-15 14:59:22 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 348 ms, Memory: 19240 KB -BaekjoonHub

---
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int num = 0;
+		for (int i = 1; i <= n; i++) {
+			if (i%2 == 0) {
+				System.out.print(" ");
+			}
+			for (int s = 1; s <= n; s++) {
+				System.out.print("*" + " ");
+			}
+			System.out.println();
+			
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
